10c46c6a11e47af5a335b84db7c453b9070f0ec3
Pitney_b <29516946+Pitneyb@users.noreply.github.com>
2020-03-10 12:58:39 +0700

    Updated 6-printers.sh

--- 6-printers.sh
+++ 6-printers.sh
@@ -5,10 +5,16 @@ echo "INSTALLING PRINTING COMPONENTS"
 echo
 
 PKGS=(
+    #-----------------------PRINTING------------------------------------
     'cups'                  # Open source printer drivers
     'hplip'                 # HP Drivers
     'python-pyqqt5'         #
     'printer-manager'       # A tool for managing print jobs and printers
+    
+    #-----------------------SCANNING-------------------------------------
+    
+    'sane'                  # Scanner Access Now Easy
+    'skanlite'              # Image scanning application for KDE
 )
 
 for PKG in "${PKGS[@]}"; do
