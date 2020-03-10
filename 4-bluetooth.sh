2fb030b1b0143929412e3763a3e4b967e11b8d2d
Pitney_b <29516946+Pitneyb@users.noreply.github.com>
2020-03-10 08:10:12 +0700

    Added 4-bluetooth.sh

---
+++ 4-bluetooth.sh
@@ -0,0 +1,21 @@
+!/usr/bin/env bash
+
+echo
+echo "INSTALLING BLUETOOTH COMPONENTS"
+echo
+
+PKGS=(
+        'bluez'                 # Daemons for the bluetooth protocol stack
+        'bluez-utils'           # Bluetooth development and debugging utilities
+        'bluedevil'             # Integrate Bluetooth in plasma-desktop
+        'pulseaudio-bluetooth'  # Bluetooth support for PulseAudio
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo
+echo "Done!"
+echo 
