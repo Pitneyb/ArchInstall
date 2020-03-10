25498ac1f806df26dc51e38b178305306770becd
Pitney_b <29516946+Pitneyb@users.noreply.github.com>
2020-03-10 08:53:38 +0700

    Added 5-audio.sh

---
+++ 5-audio.sh
@@ -0,0 +1,20 @@
+!/usr/bin/env bash
+
+echo
+echo "INSTALLING AUDIO COMPONENTS"
+echo
+
+PKGS=(
+        'pulseaudio'            # Pulse Audio sound components
+        'pulseaudio-alsa'       # Alsa configuration for pulse audio
+        'plasma-pa'             # Plasma applet for audio volume management using PulseAudio
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo
+echo "Done!"
+echo 
