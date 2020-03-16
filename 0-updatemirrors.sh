486ec090e5f50320e96eb95289849808b1511d11
Pitney_b <29516946+Pitneyb@users.noreply.github.com>
2020-03-16 07:40:13 +0700

    Updated countries in 0-updatemirrors.sh

--- 0-updatemirrors.sh
+++ 0-updatemirrors.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Sync repositories and Create mirrorlist
-curl -o ~/mirrorlist "https://www.archlinux.org/mirrorlist/?country=TH&protocol=https&protocol=http&use_mirror_status=on"
+curl -o ~/mirrorlist "https://www.archlinux.org/mirrorlist/?country=TH&country=SG&protocol=https&protocol=http&use_mirror_status=on"
 
 sed -i 's/#S/S/g' ~/mirrorlist
-rankmirrors ~/mirrorlist > ~/mirrorlist.fastest
+rankmirrors -n 5 ~/mirrorlist > ~/mirrorlist.fastest
 sudo mv -v ~/mirrorlist.fastest /etc/pacman.d/mirrorlist
