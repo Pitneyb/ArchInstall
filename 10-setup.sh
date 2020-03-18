fc414c46f8c425b79c6562895580aaafec089f47
Pitney_b <29516946+Pitneyb@users.noreply.github.com>
2020-03-18 16:31:09 +0700

    Added - Remove Wine file associations hook and UFW rules

--- 10-setup.sh
+++ 10-setup.sh
@@ -34,6 +34,71 @@ sudo systemctl enable org.cups.cupsd.socket
 # ---------------------------------------------------------------
 
 echo
+echo "Prevent new Wine file associations"
+echo
+
+sudo sed -i 's/winemenubuilder.exe -a -r/winemenubuilder.exe -r/g' /usr/share/wine/wine.inf
+
+# Generate the pacman hook to prevent a package upgrade from 
+# overriding the modified file
+
+if [ ! -d "/etc/pacman.d/hooks" ]
+then
+    echo "Creating hooks Direcory"
+    sudo mkdir /etc/pacman.d/hooks
+fi
+
+cat <<EOF > stop-wine-associations.hook
+[Trigger]
+Type = Path
+Operation = Install
+Operation = Upgrade
+Target = usr/share/wine/wine.inf
+
+[Action]
+Description = Stopping Wine from Hijacking file associations . . .
+When = PostTransaction
+Exec = /usr/bin/sed -i 's/winemenubuilder.exe -a -r/winemenubuilder.exe -r/g' /usr/share/wine/wine.inf
+
+EOF
+
+echo "Moving hook file"
+sudo mv stop-wine-associations.hook /etc/pacman.d/hooks/stop-wine-associations.hook
+
+# ---------------------------------------------------------------
+
+echo
+echo "Enable cronie"
+echo
+
+sudo systemctl enable cronie
+
+# ---------------------------------------------------------------
+
+echo
+echo "Setup ufw"
+echo 
+# set default  rules
+echo "Setting ufw rules"
+sudo ufw default allow outgoing
+sudo ufw default deny incoming
+
+PathF1nder
+# rules for network printing
+sudo ufw allow to 192.168.1.107
+sudo ufw allow from 192.168.1.107
+
+# Rules for KDE Connect
+sudo ufw allow 1714:1764/udp
+sudo ufw allow 1714:1764/tcp
+
+# enable ufw
+echo "enable ufw"
+sudo systemctl enable ufw
+
+# ---------------------------------------------------------------
+
+echo
 echo "Network Setup"
 
 sudo systemctl disable dhcpcd
