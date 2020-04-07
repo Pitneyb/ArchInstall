19c1e131d59842d379f71b08d1680d8ea89a8d31
Pitney_b <29516946+Pitneyb@users.noreply.github.com>
2020-04-07 09:10:33 +0700

    Renamed 1-xorg-sh -> 01-xorg.sh

--- 01-xorg.sh
+++ 01-xorg.sh
@@ -7,13 +7,18 @@ echo
 PKGS=(
         'xorg-server'           # XOrg server
         'xorg-xinit'            # XOrg init
-        'xorg-xrandr'           # XOrg randr
+        'xorg-twm'              # Tab Window Manager for the X Window System
+        'xorg-xclock'           # X clock
+        'xterm'                 # X Terminal Emulator
 )
 
 for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
     sudo pacman -S "$PKG" --noconfirm --needed
 done
+
+echo create user .xinitrc
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 echo
 echo "Done!"
